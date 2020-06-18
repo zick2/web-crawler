9e2046c948b21185c789cc51efd7745267b62a7e
sadiq <sadiq4wans@gmail.com>
2020-06-17 21:27:12 -0500

    able to extract urls

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,38 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
 
 public class Main {
     public  static void main(String[] args){
-        System.out.println("Hello ... ");
+        System.out.println("Crawler ... ");
 
-        //Getting some simple HTML ...
-        String html = "<html><head><title>First parse</title></head>"
-                + "<body><p id=\"txt\">Parsed HTML into a doc.</p></body></html>";
-        //Parsing HTML using Jsoup ...
-        Document doc = Jsoup.parse(html);
+        /**
+         * Websites for Testing:
+         * 1) http://www.sciencefix.com/
+         * 2) http://kauaimark.blogspot.com/
+         * 3) https://www.miss-thrifty.co.uk
+         * */
+        String start_url = "http://www.sciencefix.com/";
 
-        //Extracting an element using Id ...
-         Element elem = doc.getElementById("txt");
-        System.out.print("Extracted element: \n"+ elem);
+        try{
+            //Fetching and parsing HTMl file ...
+            Document doc = Jsoup.connect(start_url).get();
+            //Extracting all <a> tags
+            Elements links = doc.select("a[href]");
+            //For each tag extract the href attribute (it contains the urls we need)
+            for(Element url : links){
+                System.out.println("url : "+url.attr("abs:href"));
+            }
+
+        }catch (IOException IO_error){
+            System.out.println("ERROR:");
+            IO_error.printStackTrace();
+        }
+
     }
 
 
