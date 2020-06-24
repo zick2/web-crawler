5df866b97522bb6b32d87844dc8add21414cfd6d
sadiq <sadiq4wans@gmail.com>
2020-06-24 14:21:59 -0500

    todo

--- src/main/java/Spider.java
+++ src/main/java/Spider.java
@@ -4,30 +4,39 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Spider {
     //TODO :
-    // - Make http requests and extract urls recursively from sites
-    // - process and Store each url
-    // - scrape general and specific data [tables, and specified CSS selectors or HTML tags]
-    // - Create a robots.txt file reader method (This will stop the spider from violating website crawling rules)
+    // - Make http request and extract urls from site url
+    //     * Create a robots.txt file reader method (This will stop the spider from violating website crawling rules)
+    //     * Look for XML sitemaps (They usually contain all the site urls)
+    // - Preprocess and Store each extracted url :
+    //      * Collect urls that are in the target domain [Optional]
+    //      * Make sure it deos not exist in the crawled list
+    // [Repeat]
 
-    public Spider(){}
+    ArrayList<String> unCrawled_Urls = new ArrayList<String>();
+    ArrayList<String> crawled_Urls = new ArrayList<String>();
 
-    public void crawl(String url){
+    public Spider() {
+    }
+
+    public void crawl(String url) {
+
         System.out.println("Crawling ...");
-        try{
-            //Fetching and parsing HTMl ...
-            Document doc = Jsoup.connect(url).get();
-            //Extracting all <a href=""> tags
-            Elements links = doc.select("a[href]");
-            //For each <a href=""> tag extract the href attribute (it contains the urls we need)
 
-            for(Element link : links){
-                System.out.println("url : "+ link.attr("abs:href"));
-            }
-            System.out.println( links.size() +" uls");
-        } catch (IOException IO_error){
+        try {
+            // crawled_Urls.add(url);
+                Document doc = Jsoup.connect(url).get(); // Fetching and parsing HTMl
+                Elements links = doc.select("a[href]"); // Extracting all <a href=""> tags
+
+                // For each <a href=""> tag extract the href attribute (it contains the urls we need)
+                for (Element link : links) {
+                    System.out.println("url : " + link.attr("abs:href"));
+                }
+                System.out.println(links.size() + " uls");
+        } catch (IOException IO_error) {
             System.out.println("ERROR:");
             IO_error.printStackTrace();
         }
