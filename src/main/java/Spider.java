c8c4fec8696cb9d5626880b4205ac39e6db4d691
sadiq <sadiq4wans@gmail.com>
2020-06-26 18:59:51 -0500

    Added some preprocessing on urls and recursion
    The crawler is now able to recursivily crawl urls, also preprocessing is added though there is still some more steps to add to make the preprocessing perffect to handle 400 errors

--- src/main/java/Spider.java
+++ src/main/java/Spider.java
@@ -11,31 +11,82 @@ public class Spider {
     // - Make http request and extract urls from site url
     //     * Create a robots.txt file reader method (This will stop the spider from violating website crawling rules)
     //     * Look for XML sitemaps (They usually contain all the site urls)
-    // - Preprocess and Store each extracted url :
+    //     * Add crawled url to crawled_url list (if it exists in uncrawled url list delete it)
+    // - Preprocess and Store each extracted url (DB -> CSV file):
     //      * Collect urls that are in the target domain [Optional]
     //      * Make sure it deos not exist in the crawled list
     // [Repeat]
 
-    ArrayList<String> unCrawled_Urls = new ArrayList<String>();
+    ArrayList<String> uncrawled_Urls = new ArrayList<String>();
     ArrayList<String> crawled_Urls = new ArrayList<String>();
+    public boolean TERMINATE = false;
 
     public Spider() {
     }
 
     public void crawl(String url) {
 
-        System.out.println("Crawling ...");
+        System.out.println(url);
 
         try {
-            // crawled_Urls.add(url);
+            if (!TERMINATE) {
+                // uncrawled_Urls.add(url);
                 Document doc = Jsoup.connect(url).get(); // Fetching and parsing HTMl
+                crawled_Urls.add(url);// Add current url to crawled list
+
                 Elements links = doc.select("a[href]"); // Extracting all <a href=""> tags
 
-                // For each <a href=""> tag extract the href attribute (it contains the urls we need)
+                //- For each element OR <a href=""> tag ...
                 for (Element link : links) {
-                    System.out.println("url : " + link.attr("abs:href"));
+                    // Extract the absolute href attribute (it contains the url we need) ...
+                    String l = link.attr("abs:href");
+
+                    /********************************************************************************************
+                     * Preprocess Urls
+                     * - url contains domain name e.g example.com, continue preprocessing
+                     * - url does not exist in uncrawled_url and crawled_url list, add it to the uncrawled list
+                     * - Remove it from the uncrawled list, if it exists
+                     ***/
+
+                    // ADD MORE PREPROCESSING CODE HERE ...
+
+                    // If url does not exist in uncrawled_url and crawled list ...
+                    if (!uncrawled_Urls.contains(l) && !crawled_Urls.contains(l)) {
+                        uncrawled_Urls.add(l);//Then add it to unCrawled_url list
+                    }// BUT if url exists in uncrawled_url list ...
+                    else if (uncrawled_Urls.contains(l)) {
+                        uncrawled_Urls.remove(l);  // Then remove it
+                    }
+
+                    // ADD MORE PREPROCESSING CODE HERE ...
+
+                }//for Each loop END
+
+                /******************************
+                 *  Selecting new url to crawl
+                 *
+                 * */
+
+                //If uncrawled list is not empty ...
+                if (!uncrawled_Urls.isEmpty()) {
+                    //And also, if uncrawled list has more than 1 element or urls ...
+                    if (uncrawled_Urls.size() > 1) {
+                        // Then generate random index (0 - list size)
+                        int randomIndex = (int) (Math.random() * uncrawled_Urls.size());
+                        // Then use random index to select element or url from list
+                        String new_url = uncrawled_Urls.get(randomIndex);
+                        crawl(new_url);  // Begin recursion
+                    }
+                } else {
+                    TERMINATE = true;
+                    System.out.println("No more urls in uncrawled list: \n Spider is terminating ...");
                 }
-                System.out.println(links.size() + " uls");
+
+            } // End of while loop
+
+            System.out.println(uncrawled_Urls.size() + " urls uncrawled ");
+            System.out.println(crawled_Urls.size() + " urls crawled ");
+
         } catch (IOException IO_error) {
             System.out.println("ERROR:");
             IO_error.printStackTrace();
