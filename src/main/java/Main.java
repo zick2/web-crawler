fdede01444a47cccee2e12032f484a30450ecb89
sadiq <sadiq4wans@gmail.com>
2020-06-29 19:55:41 -0500

    Re-organized code and added error handling

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,13 +3,14 @@ public class Main {
 
         /**
          * Websites for Testing:
-         * 1) http://www.sciencefix.com/
-         * 2) http://kauaimark.blogspot.com/
-         * 3) https://www.miss-thrifty.co.uk
+         *  https://asoftmurmur.com/
+         *  http://www.sciencefix.com/
+         *  http://kauaimark.blogspot.com/
+         *  https://www.miss-thrifty.co.uk
          * */
 
-        String start_url = "http://kauaimark.blogspot.com/";
-        String domain = "kauaimark.blogspot.com";
+        String start_url = "http://www.sciencefix.com/";
+        String domain = "sciencefix.com";
 
         Spider mySpider = new Spider();
         mySpider.crawl(start_url, domain);
--- src/main/java/Spider.java
+++ src/main/java/Spider.java
@@ -19,9 +19,18 @@ public class Spider {
     //      * Make sure it deos not exist in the crawled list
     // [Repeat]
 
+    // TODO: Error Handling
+    //  - java.io.IOException:
+    //  - org.jsoup.HttpStatusException: HTTP error fetching URL. Status=400
+    //  - javax.net.ssl.SSLHandshakeException:
+    //  - java.net.MalformedURLException: Only http & https protocols supported
+
     public ArrayList<String> uncrawled_Urls = new ArrayList<String>();
     public ArrayList<String> crawled_Urls = new ArrayList<String>();
-    public boolean TERMINATE = false;
+
+    public int NUM_OF_ERRORS = 0;
+    public int TOTAL_NUM_OF_URLS = 0;
+    public int NUM_OF_BAD_URLS = 0;
 
     public int i = 0;
 
@@ -30,110 +39,91 @@ public class Spider {
     }
 
     /**
+     * -----------
      * crawl()
      */
     public void crawl(String url, String domain) {
-        if (!TERMINATE) {
-            try {
-                // uncrawled_Urls.add(url);
+        try {
+            // Fetching and parsing HTMl ...
+            Document doc = Jsoup.connect(url)
+                    .ignoreContentType(true)
+                    //.ignoreHttpErrors(true)
+                    .get();
+            Elements links = doc.select("a[href]"); // Extracting all <a href=""> tags
 
-                // Fetching and parsing HTMl ...
-                Document doc = Jsoup.connect(url)
-                        .ignoreContentType(true)
-                        .ignoreHttpErrors(true)
-                        .get();
+            if (!links.isEmpty()) {
+                crawled_Urls.add(url);// Add current url to crawled list
+                System.out.println((i++) + ": " + url);
 
-                if (doc != null) {
-                    crawled_Urls.add(url);// Add current url to crawled list
-                    Elements links = doc.select("a[href]"); // Extracting all <a href=""> tags
+                /**-------------------------------------------------------------------------------------------
+                 * Preprocess Urls
+                 * - url contains domain name e.g example.com, continue preprocessing
+                 * - url does not exist in uncrawled_url and crawled_url list, add it to the uncrawled list
+                 * - Remove it from the uncrawled list, if it exists
+                 ***/
 
-                    System.out.println((i++) + ": " + url);
+                //- For each element OR <a href=""> tag ...
+                for (Element link : links) {
+                    TOTAL_NUM_OF_URLS++;
+                    // Extract the absolute href attribute (it contains the url we need) ...
+                    String l = link.attr("abs:href");
 
-                    /**-------------------------------------------------------------------------------------------
-                     * Preprocess Urls
-                     * - url contains domain name e.g example.com, continue preprocessing
-                     * - url does not exist in uncrawled_url and crawled_url list, add it to the uncrawled list
-                     * - Remove it from the uncrawled list, if it exists
-                     ***/
+                    if (l.contains(domain) && l.contains("http")) {
+                        // TODO - ADD MORE PREPROCESSING CODE HERE ...
 
-                    //- For each element OR <a href=""> tag ...
-                    for (Element link : links) {
-                        // Extract the absolute href attribute (it contains the url we need) ...
-                        String l = link.attr("abs:href");
-
-                        if (l != "" && l.contains(domain)) {
-                            // TODO - ADD MORE PREPROCESSING CODE HERE ...
-
-                            // If url does not exist in uncrawled_url and crawled list ...
-                            if (!uncrawled_Urls.contains(l) && !crawled_Urls.contains(l)) {
-                                uncrawled_Urls.add(l);//Then add it to unCrawled_url list
-                            }// BUT if url exists in uncrawled_url list ...
-                            else if (uncrawled_Urls.contains(l)) {
-                                uncrawled_Urls.remove(l);  // Then remove it
-                            }
-
-                            // TODO - ADD MORE PREPROCESSING CODE HERE ...
-                        } else {
-                            System.out.println("Url out of domain: " + l);
+                        // If url does not exist in uncrawled_url and crawled list ...
+                        if (!uncrawled_Urls.contains(l) && !crawled_Urls.contains(l)) {
+                            uncrawled_Urls.add(l);//Then add it to unCrawled_url list
+                        }// BUT if url exists in uncrawled_url list ...
+                        else if (uncrawled_Urls.contains(l)) {
+                            uncrawled_Urls.remove(l);  // Then remove it
                         }
 
-                    }//for Each loop END
+                        // TODO - ADD MORE PREPROCESSING CODE HERE ...
+                    } else {
+                        NUM_OF_BAD_URLS++;
+                        System.out.println("Out_of_Domain: " + l);
+                    }
+                }//for Each loop END
+            } // End of if: !links.isEmpty()
 
-                    //--------------------------
+        } catch (Exception e) {
+            NUM_OF_ERRORS++;
+            //Handle all Exceptions here ...
+            System.out.println("Caught Error: " + e);
 
-                    this.crawlNextURL(domain);
-                } else {
-                    System.out.print("Null Document");
-                } // End of if else: null checkpoint
-            }// TODO: Error Handling
-             //  - java.io.IOException:
-             //  - org.jsoup.HttpStatusException: HTTP error fetching URL. Status=400
-             //  - javax.net.ssl.SSLHandshakeException:
-            catch (Exception e) {
-                //Handle all Exceptions here ...
-                System.out.println("Caught Error: "+ e);
-            } finally {
-                //System.out.println("Fixed error");
-               // this.crawlNextURL(domain);
-            }
-            //---------------------------------------
-//            catch (IOException IO_error) {
-//                this.printCrawlStatus();
-//                System.out.println("IO_error");
-//                IO_error.printStackTrace();
-//            } catch (HandlerException http_error) {
-//                this.printCrawlStatus();
-//                System.out.println("http_error");
-//                http_error.printStackTrace();
-//            }
+            //---------------------------------------------
+            this.crawlNextURL(domain); // Recursion point, if exception occurs
+        }
 
-        } else {
-            this.printCrawlStatus();
-        } // End of if else:  termination point
-    }
+        //--------------------------------------------
+        this.crawlNextURL(domain); // Recursion point
+    } // End of crawl()
 
     public void printCrawlStatus() {
-        System.out.println(uncrawled_Urls.size() + " urls uncrawled ");
-        System.out.println(crawled_Urls.size() + " urls crawled ");
+        System.out.println(uncrawled_Urls.size() + " Uncrawled urls, " + crawled_Urls.size() + " Crawled urls");
+        System.out.println("Total urls: " + TOTAL_NUM_OF_URLS + ", Bad urls: " + NUM_OF_BAD_URLS + ", Errors: " + NUM_OF_ERRORS);
     }
 
     /**
+     * ---------------
      * crawlNextUrl()
      */
     public void crawlNextURL(String domain) {
         //If uncrawled list is not empty ...
         if (!uncrawled_Urls.isEmpty()) {
-            //And also, if uncrawled list has more than 1 element or urls ...
-            if (uncrawled_Urls.size() > 1) {
-                // Then generate random index (0 - list size)
-                int randomIndex = (int) (Math.random() * uncrawled_Urls.size());
-                // Then use random index to select element or url from list
-                String new_url = uncrawled_Urls.get(randomIndex);
-                this.crawl(new_url, domain);  // Begin recursion
-            }
+            // Then generate random index (0 - list size)
+            int randomIndex = (int) (Math.random() * uncrawled_Urls.size());
+            // Then use random index to select element or url from list
+            String new_url = uncrawled_Urls.get(randomIndex);
+            uncrawled_Urls.remove(randomIndex); // After getting the url, remove it from the list
+
+            //-----------------------------------------------
+            this.crawl(new_url, domain);  // Begin recursion
         } else {
-            TERMINATE = true;
-            System.out.println("No more urls in uncrawled list: \n Spider is terminating ...");
+            System.out.println("Spider has terminated!!");
+            this.printCrawlStatus();
+            // TERMINATE = true;
         }
     }
 }
